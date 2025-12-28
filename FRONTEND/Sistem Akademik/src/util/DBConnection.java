2b08abaadec2e54b5e38f900cea4725bb3e32ebd
rynnnoffin-dot <rynnnoffin@gmail.com>
2025-12-28 23:52:19 +0700

    update

--- FRONTEND/Sistem Akademik/src/util/DBConnection.java
+++ FRONTEND/Sistem Akademik/src/util/DBConnection.java
@@ -6,13 +6,13 @@ import java.sql.DriverManager;
 public class DBConnection {
 
     private static final String URL =
-            "jdbc:mysql://localhost:3306/eightplanner?useSSL=false&serverTimezone=UTC";
+        "jdbc:mysql://localhost:3306/eightplanner?useSSL=false&serverTimezone=UTC";
     private static final String USER = "root";
     private static final String PASS = "";
 
     public static Connection getConnection() {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver"); // WAJIB
+            Class.forName("com.mysql.cj.jdbc.Driver");
             return DriverManager.getConnection(URL, USER, PASS);
         } catch (Exception e) {
             e.printStackTrace();
