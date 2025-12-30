59808b70fca2f4fdc061f66bd26b1f375403d63d
rynnnoffin-dot <rynnnoffin@gmail.com>
2025-12-30 13:56:23 +0700

    update

---
+++ FRONTEND/Sistem Akademik/src/TestFX.java
@@ -0,0 +1,20 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class TestFX extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        Label label = new Label("JavaFX OK 🚀");
+        Scene scene = new Scene(label, 300, 200);
+        stage.setScene(scene);
+        stage.setTitle("Test JavaFX");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
